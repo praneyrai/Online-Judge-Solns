fd43ace474c766f0d54f3d27605099d93dcef2fe
Praney Rai <praney86@gmail.com>
2017-06-13 22:41:08 +0530

    Games

---
+++ SPOJ/IITKWPCN.cpp
@@ -0,0 +1,13 @@
+    #include "bits/stdc++.h"
+     
+    using namespace std;
+     
+    int main() {
+    	int t; cin >> t;
+    	while (t--) {
+    		int w, b;
+    		cin >> w >> b;
+    		if (b%2) cout << "1.000000\n";
+    		else cout << "0.000000\n";		
+    	}
+    } 
